6c0fb8fa6e090678e19b019ce333ef83a570f53a
Your Name <you@example.com>
2021-01-20 02:02:19 +0600

    DZ29

--- main/views.py
+++ main/views.py
@@ -6,5 +6,8 @@ def homepage(request):
 def test(request):
     return render(request, "test.html")
 
-def second(request):
-    return HttpResponse("test 2 page")
+#def second(request):
+    #return HttpResponse("test 2 page")
+
+def third(request):
+    return HttpResponse("This is page test3")
