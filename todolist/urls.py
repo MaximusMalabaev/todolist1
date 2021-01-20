0af144b66a30434808cd907a40f713c471ae5f4b
Your Name <you@example.com>
2021-01-21 01:30:32 +0600

    Dz31

--- todolist/urls.py
+++ todolist/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     #path("test2/", second)
     path("test3/", third)
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
